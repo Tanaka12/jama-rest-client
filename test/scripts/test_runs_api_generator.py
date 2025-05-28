660e1606f25d1071eb288aa12d494bbb1c1f0aa1
jlpeiro <jpeiro@werfen.com>
2025-05-28 15:19:29 +0200

    Added script to ease the generation of test runs API responses

---
+++ test/scripts/test_runs_api_generator.py
@@ -0,0 +1,70 @@
+import json
+
+ITEMS_COUNT: int = 30
+
+if __name__ == "__main__":
+
+    result = \
+    {
+        "meta": {
+        "status": "OK",
+        "timestamp": "2025-05-22T15:46:31.191+0000",
+        "pageInfo": {
+            "startIndex": 0,
+            "resultCount": 0,
+            "totalResults": 0
+        }
+        },
+        "links": {
+            "data.fields.pickList": {
+                "type": "picklists",
+                "href": ""
+            },
+            "data.fields.itemType": {
+                "type": "itemtypes",
+                "href": ""
+            }
+        }
+    }
+
+    data = []
+    for index in range(1, ITEMS_COUNT):
+        element = \
+        {
+            'id': index,
+            'documentKey': f'DummyDocumentKey {index}',
+            'globalId': f'DummyGlobalId {index}',
+            'itemType': index + 1,
+            'project': index + 2,
+            'createdDate': f'DummyCreatedDate {index}',
+            'modifiedDate': f'DummyModifiedDate {index}',
+            'lastActivityDate': f'DummyLastActivityDate {index}',
+            'createdBy': index + 3,
+            'modifiedBy': index + 4,
+            "testCaseVersionNumber": index + 5,
+            "testCaseCurrentVersionNumber": index + 6,
+            "sortOrderFromTestGroup": index + 7,
+            "fields": {
+                "fieldStr": "DummyField",
+                "fieldInt": 0,
+                "fieldBool": True
+            },
+            "resources": {
+                "self": {
+                    "allowed": [
+                        "GET",
+                        "PUT",
+                        "PATCH",
+                        "DELETE"
+                    ]
+                }
+            },
+            "type": "testruns"
+        }
+
+        data.append(element)
+
+    result['data'] = data
+
+    with open('file.json', 'w') as file_handle:
+        file_handle.write(json.dumps(result))
