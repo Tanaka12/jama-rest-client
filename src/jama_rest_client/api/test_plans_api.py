2183f7d73b3269bb9d9f915d8ecf92db80ed37e7
jlpeiro <jpeiro@werfen.com>
2025-05-28 14:15:26 +0200

    Added method on test plans API to gather a test plan cycles

--- src/jama_rest_client/api/test_plans_api.py
+++ src/jama_rest_client/api/test_plans_api.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from jama_rest_client.http import HTTPClient
-from jama_rest_client.dal.parsers.json import TestPlanJSONParser
+from jama_rest_client.dal.parsers.json import TestCycleJSONParser, TestPlanJSONParser
+from jama_rest_client.model.test_cycle import TestCycle
 from jama_rest_client.model.test_plan import TestPlan
 
 from .base_api import BaseAPI
@@ -27,5 +28,23 @@ class TestPlansAPI(BaseAPI):
 
         return test_plans
     
+    def get_test_plan_cycles(self, test_plan_id: int) -> List[TestCycle]:
+        test_cycles: List[TestCycle] = []
+
+        start_index: int = 0
+        while True:
+            test_cycles_batch = self.__parse_test_cycles_dict(self._get(f'{self.__resourceName}/{test_plan_id}/cycles?startAt={start_index}&maxResults=50').body['data'])
+            
+            if len(test_cycles_batch) == 0:
+                break
+
+            test_cycles.extend(test_cycles_batch)
+            start_index += len(test_cycles_batch)
+
+        return test_cycles
+
     def __parse_test_plans_dict(self, test_plans_list_dict: List[dict]) -> List[TestPlan]:
         return list(map(lambda test_plan_dict: TestPlanJSONParser.parse(test_plan_dict), test_plans_list_dict))
+
+    def __parse_test_cycles_dict(self, test_cycles_list_dict: List[dict]) -> List[TestCycle]:
+        return list(map(lambda test_cycle_dict: TestCycleJSONParser.parse(test_cycle_dict), test_cycles_list_dict))
