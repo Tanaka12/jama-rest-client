ff44c7a11a230a4d109a4127e52edd4e2e44dbb5
jlpeiro <jpeiro@werfen.com>
2025-06-10 19:30:06 +0200

    Added comparison methods on api response elements

--- src/jama_rest_client/model/api_response/api_response.py
+++ src/jama_rest_client/model/api_response/api_response.py
@@ -1,3 +1,5 @@
+from typing_extensions import Self
+
 class APICreateResponse:
     status: int
     status_reason_phrase: str
@@ -10,6 +12,15 @@ class APICreateResponse:
         self.location = ""
         self.id = 0
 
+    def __eq__(self, other: Self):
+        if self.status == other.status \
+        and self.status_reason_phrase == other.status_reason_phrase \
+        and self.location == other.location \
+        and self.id == other.id:
+            return True
+
+        return False
+
 class APIUpdateResponse:
     status: int
     statusResonPhrase: str
@@ -17,6 +28,13 @@ class APIUpdateResponse:
     def __init__(self):
         self.status = 0
         self.status_reason_phrase = ""
+    
+    def __eq__(self, other: Self):
+        if self.status == other.status \
+        and self.status_reason_phrase == other.status_reason_phrase:
+            return True
+
+        return False
 
 class APIDeleteResponse:
     status: int
@@ -25,3 +43,10 @@ class APIDeleteResponse:
     def __init__(self):
         self.status = 0
         self.status_reason_phrase = ""
+
+    def __eq__(self, other: Self):
+        if self.status == other.status \
+        and self.status_reason_phrase == other.status_reason_phrase:
+            return True
+
+        return False
