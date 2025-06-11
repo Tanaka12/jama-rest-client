21e475dd9e6e46bee78f2569fa064aabc431c0e1
jlpeiro <jpeiro@werfen.com>
2025-06-11 16:24:50 +0200

    Added mock for test cycle request

--- test/mocks/test_cycles/test_cycles_mock.py
+++ test/mocks/test_cycles/test_cycles_mock.py
@@ -9,11 +9,12 @@ class TestCyclesMocks(str, Enum):
     CASE_NO_ELEMENTS = "NO_ELEMENTS"
     CASE_1_ELEMENT = "1_ELEMENT"
     CASE_30_ELEMENTS = "30 ELEMENTS"
+    CASE_REQUEST = "CASE_REQUEST"
     
-
 TEST_CYCLES_API_MOCKS: dict[TestCyclesMocks, dict] = \
 {
     TestCyclesMocks.CASE_NO_ELEMENTS: json.load(open(os.path.join(API_SCENARIOS_PATH, 'test_cycles_empty.json'))),
     TestCyclesMocks.CASE_1_ELEMENT: json.load(open(os.path.join(API_SCENARIOS_PATH, '1_test_cycle.json'))),
-    TestCyclesMocks.CASE_30_ELEMENTS: json.load(open(os.path.join(API_SCENARIOS_PATH, '30_test_cycles.json')))
+    TestCyclesMocks.CASE_30_ELEMENTS: json.load(open(os.path.join(API_SCENARIOS_PATH, '30_test_cycles.json'))),
+    TestCyclesMocks.CASE_REQUEST: json.load(open(os.path.join(API_SCENARIOS_PATH, 'test_cycle_request.json'))), 
 }
