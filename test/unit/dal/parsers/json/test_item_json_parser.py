c5aec1d86ebf5795a3600cb697ea6a3a868fd5c0
jlpeiro <jpeiro@werfen.com>
2025-06-11 10:02:07 +0200

    Updated item JSON parser UT

--- test/unit/dal/parsers/json/test_item_json_parser.py
+++ test/unit/dal/parsers/json/test_item_json_parser.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytest
 
 from jama_rest_client.dal.parsers.json import ItemJSONParser
@@ -18,9 +19,10 @@ class TestItemJSONParser():
                          .set_global_id('DummyGlobalId 1')
                          .set_item_type(2)
                          .set_project(3)
-                         .set_created_date('DummyCreatedDate 1')
-                         .set_modified_date('DummyModifiedDate 1')
-                         .set_last_activity_date('DummyLastActivityDate 1')
+                         .set_child_item_type(6)
+                         .set_created_date(datetime.fromtimestamp(1582199426))
+                         .set_modified_date(datetime.fromtimestamp(1582199426))
+                         .set_last_activity_date(datetime.fromtimestamp(1582199426))
                          .set_created_by(4)
                          .set_modified_by(5)
                          .set_lock(
@@ -50,9 +52,10 @@ class TestItemJSONParser():
                          .set_global_id('DummyGlobalId 1')
                          .set_item_type(2)
                          .set_project(3)
-                         .set_created_date('DummyCreatedDate 1')
-                         .set_modified_date('DummyModifiedDate 1')
-                         .set_last_activity_date('DummyLastActivityDate 1')
+                         .set_child_item_type(6)
+                         .set_created_date(datetime.fromtimestamp(1582199426))
+                         .set_modified_date(datetime.fromtimestamp(1582199426))
+                         .set_last_activity_date(datetime.fromtimestamp(1582199426))
                          .set_created_by(4)
                          .set_modified_by(5)
                          .set_lock(
