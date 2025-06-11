3be0f4abcdf1284a6312330be3a2ddebfb361208
jlpeiro <jpeiro@werfen.com>
2025-06-11 17:09:44 +0200

    Added call update_test_plan on test plans API

--- src/jama_rest_client/api/test_plans_api.py
+++ src/jama_rest_client/api/test_plans_api.py
@@ -3,13 +3,14 @@ from typing import List
 from jama_rest_client.http import HTTPClient
 from jama_rest_client.dal.parsers.json import \
 (
+    AbstractRestResponseJSONParser,
     CreatedResponseJSONParser, 
     TestCycleJSONParser, 
     TestGroupJSONParser, 
     TestPlanJSONParser
 )
 from jama_rest_client.dal.serializers.json import TestCycleRequestJSONSerializer, TestPlanRequestJSONSerializer
-from jama_rest_client.model.api_response import CreatedResponse
+from jama_rest_client.model.api_response import AbstractRestResponse, CreatedResponse
 from jama_rest_client.model.test_cycle import TestCycle, TestCycleRequest
 from jama_rest_client.model.test_plan import TestGroup, TestPlan, TestPlanRequest
 
@@ -43,7 +44,11 @@ class TestPlansAPI(BaseAPI):
     def get_test_plan(self, test_plan_id: int) -> TestPlan:
         test_plan_dict = self._get(f'{self.__resourceName}/{test_plan_id}').body['data']
         return TestPlanJSONParser.parse(test_plan_dict)
-        
+    
+    def update_test_plan(self, test_plan_id: int, test_plan_request: TestPlanRequest) -> AbstractRestResponse:
+        http_response = self._put(f'{self.__resourceName}/{test_plan_id}', TestPlanRequestJSONSerializer.serialize(test_plan_request))
+        return AbstractRestResponseJSONParser.parse(http_response.body)
+
     def get_test_plan_cycles(self, test_plan_id: int) -> List[TestCycle]:
         test_cycles: List[TestCycle] = []
 
